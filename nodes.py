b79a4a109471fad526f4fe16ecebf1d90cc97f75
Yan Carlos <urbanoyaan@gmail.com>
2026-03-31 01:33:22 -0500

    feat: add search and delete functionality

--- nodes.py
+++ nodes.py
@@ -32,6 +32,49 @@ class BinarySearchTree:
         else:
             print(f"Value already exists ({value})")
             
+    def search(self, value):
+        return self._search_recursive(self.root, value)
+    
+    def _search_recursive(self, current, value):
+        if current is None:
+            return False
+        
+        if current.value == value:
+            return True
+        elif value < current.value:
+            return self._search_recursive(current.left, value)
+        else:
+            return self._search_recursive(current.right, value)
+        
+        
+    def delete(self, value):
+        return self._delete_recursive(self.root, value)
+  
+    def _delete_recursive(self, current, value):
+        if current in None:
+            return False
+        
+        if value < current.value:
+            current.left = self._delete_recursive(current.left, value)
+        elif value > current.value:
+            current.right = self._delete_recursive(current.right, value)
+        else:
+            if current.left is None:
+                return current.right
+            elif current.right is None:
+                return current.left
+            
+            current.value = self._min_value_node(current.right).value
+            current.right = self._delete_recursive(current.right, current.value)
+            
+        return current
+    
+    def _min_value_node(self, node):
+        current = node
+        while current.left is not None:
+            current = current.left
+        return current
+        
     def display(self, node=None, level=0):
         if node is None and level == 0:
             node = self.root
